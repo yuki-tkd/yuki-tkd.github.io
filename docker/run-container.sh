2707756bd1ef8cea20b8d191bb77e2317bba8c09
Yuki Takeda <yuki@imaginaryshort.com>
2023-03-19 21:49:22 +0900

    20230319

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 docker run --rm -it \
 	-v $(pwd):/src \
+  -u `id -u`:`id -g` \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
 	-p 1313:1313 \
 	klakegg/hugo:0.101.0-ubuntu \
 	shell
